3fa4d81d43fc7d8b2f01b8d15b03b91f51594bca
Steffen Uhlig <steffen@familie-uhlig.net>
2018-06-01 14:30:11 +0200

    Add tracing and verbose output

--- lib/httpspell/spider.rb
+++ lib/httpspell/spider.rb
@@ -10,11 +10,12 @@ module HttpSpell
   class Spider
     attr_reader :todo, :done
 
-    def initialize(starting_point, base_url = starting_point)
+    def initialize(starting_point, base_url = starting_point, tracing: false)
       @todo = []
       @done = []
       todo << Addressable::URI.parse(starting_point)
       @base_url = Addressable::URI.parse(base_url)
+      @tracing = tracing
     end
 
     def start
@@ -26,12 +27,14 @@ module HttpSpell
             yield u, d if block_given?
           rescue
             warn "Callback error for #{url}: #{$ERROR_INFO}"
+            warn $ERROR_INFO.backtrace if @tracing
           end
 
           done.append(url)
           todo.concat(extracted - done - todo)
         rescue StandardError
           warn "Could not fetch #{url}: #{$ERROR_INFO}"
+          warn $ERROR_INFO.backtrace if @tracing
         end
       end
     end
@@ -40,17 +43,23 @@ module HttpSpell
 
     def links(uri)
       # We are using open-uri, which follows redirects and also provides the content-type.
-      response = URI(uri).read
-      return [] unless response.content_type == 'text/html'
+      response = open(uri).read
+
+      if response.respond_to?(:content_type)
+        return [] unless response.content_type == 'text/html'
+      end
+
       doc = Nokogiri::HTML(response)
 
       links = doc.css('a[href]').map do |e|
         link = Addressable::URI.parse(e['href'])
         link = uri.join(link) if link.relative?
         next unless link.to_s.start_with?(@base_url.to_s)
+        # TODO Ignore same page links (some some anchor)
         link
       rescue StandardError
-        warn $ERROR_INFO
+        warn $ERROR_INFO.message
+        warn $ERROR_INFO.backtrace if @tracing
       end.compact
 
       yield uri, doc if block_given?
